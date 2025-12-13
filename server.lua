5e7034a03a58b12bc56442289e6b0fba2e4db816
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-12-12 23:12:32 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -11,3 +11,20 @@ AddEventHandler("trap_snow:addSnowball", function()
         })
     end
 end)
+
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if resourceName ~= GetCurrentResourceName() then return end
+
+    print('^4████████╗██████╗  █████╗ ██████╗ ')
+    print('^4╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗')
+    print('^4   ██║   ██████╔╝███████║██████╔╝')
+    print('^4   ██║   ██╔══██╗██╔══██║██╔═══╝ ')
+    print('^4   ██║   ██║  ██║██║  ██║██║     ')
+    print('^4   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ')
+    print('^7')
+    print('^6   Discord : https://discord.gg/rjjU2y93X7')
+    print('^8   Créé par TRAPZY')
+    print('^3   © TRAP Development')
+
+end)
